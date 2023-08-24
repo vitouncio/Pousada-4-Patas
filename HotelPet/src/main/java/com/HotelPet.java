17277e0d99b9eef994ddd7ecb22e319d54820a7a
vitouncio <79331105+vitouncio@users.noreply.github.com>
2023-08-24 08:51:20 -0300

    Add files via upload

---
+++ HotelPet/src/main/java/com/HotelPet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com;
+
+/**
+ *
+ * @author 14892160652
+ */
+public class HotelPet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ HotelPet/src/main/java/model/Pet.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author 14892160652
+ */
+public class Pet {
+    
+}
